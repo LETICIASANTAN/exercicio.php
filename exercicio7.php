42dbd35b469a215a476d1b7d7694d88e45709063
LETICIASANTAN <leticiasantanacampelo@gmail.com>
2024-12-17 11:06:07 -0300

    Add files via upload

---
+++ exercicio7.php
@@ -0,0 +1,12 @@
+<?php
+$livro=[
+"titulo"=>"dom quixote",
+"autor"=>"miguel de cervantes",
+"anopublicacao"=> 1605,
+"preco"=>59,
+];
+echo"titulo:".$livro["titulo"]."<br>";
+echo"autor:".$livro["autor"]."<br>";
+echo"ano de publicacao:".$livro["anopublicacao"]."<br>";
+echo"preco:".$livro["preco"]."<br>";
+?>
