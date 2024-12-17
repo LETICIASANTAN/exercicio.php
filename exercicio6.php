24e5dfd9576455ea7480bdbe23af26c4c88cfb62
LETICIASANTAN <leticiasantanacampelo@gmail.com>
2024-12-17 11:05:54 -0300

    Add files via upload

---
+++ exercicio6.php
@@ -0,0 +1,16 @@
+
+
+<?php
+
+$idades=[20,26,32,34,46,48,50,54,58,60];
+
+//exibir a maior idade
+
+echo "maior idade:".max($idades)."<br>";
+
+//exibir a menor idade
+
+echo "menor idade:".min($idades);
+
+
+?>
